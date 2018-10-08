7d426fb354921776ac73ada04deb91a528489cd6
byt3_h34d <mr.tybopp@gmail.com>
2018-10-08 17:58:50 -0500

    Updating server.js scrape logic to use https://venturebeat.com/. Scrape route works.

--- server.js
+++ server.js
@@ -39,18 +39,18 @@ mongoose.connect(MONGODB_URI);
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.cnn.com/").then(function(response) {
+    axios.get("https://venturebeat.com/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article h3").each(function(i, element) {
+      // Now, we grab every h3 within an article tag, and do the following:
+      $("article h2").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
         // Add the text and href of every link, and save them as properties of the result object
         result.title = $(this)
-          .children('<span class="cd__headline-text">...</span>')
+          .children('a')
           .text();
         result.link = $(this)
           .children("a")
