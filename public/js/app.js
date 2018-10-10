698cf65c56bbd884d3a8676d9468b6be35f3bdaf
byt3_h34d <mr.tybopp@gmail.com>
2018-10-09 23:07:14 -0500

    Cannot get notes to display on index handlebars view.

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@
 //   res.render("index", articleObj);
 // });
 
-// Whenever someone clicks a p tag
+// Whenever someone clicks a View Notes button:
 $("#view-notes").on("click", function () {
   // Empty the notes from the note section
   $("#notes").empty();
@@ -27,64 +27,54 @@ $("#view-notes").on("click", function () {
     })
     // With that done, add the note information to the page
     .then(function (data) {
-      res.render("index", {
-        articles: data
-      });
-    })
-    .catch(function (err) {
-      // If an error occurred, send it to the client
-      return res.json(err);
-    });
-});
+      // The title of the article
+      $("#notes").append("<p><b>" + data.title + "</b></p>");
+      // An input to enter a new title
+      $("#notes").append("<input id='titleinput' name='title'>");
+      // A textarea to add a new note body
+      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
-
-
-// .then(function (data) {
-//   console.log(data);
-//   // The title of the article
-//   $("#notes").append("<p><b>" + data.title + "</b></p>");
-//   // An input to enter a new title
-//   $("#notes").append("<input id='titleinput' name='title'>");
-//   // A textarea to add a new note body
-//   $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-//   // A button to submit a new note, with the id of the article saved to it
-//   $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-//   // If there's a note in the article
-//   if (data.note) {
-//     // Place the title of the note in the title input
-//     $("#titleinput").val(data.note.title);
-//     // Place the body of the note in the body textarea
-//     $("#bodyinput").val(data.note.body);
-//   }
-// });
-
-
-// When you click the savenote button
-$(document).on("click", "#savenote", function () {
-  // Grab the id associated with the article from the submit button
-  var thisId = $(this).attr("data-id");
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
+      // If there's a note in the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
       }
-    })
-    // With that done
-    .then(function (data) {
-      // Log the response
-      console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
     });
 
-  // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
+
 });
+
+
+
+// // When you click the savenote button
+// $(document).on("click", "#savenote", function () {
+//   // Grab the id associated with the article from the submit button
+//   var thisId = $(this).attr("data-id");
+
+//   // Run a POST request to change the note, using what's entered in the inputs
+//   $.ajax({
+//       method: "POST",
+//       url: "/articles/" + thisId,
+//       data: {
+//         // Value taken from title input
+//         title: $("#titleinput").val(),
+//         // Value taken from note textarea
+//         body: $("#bodyinput").val()
+//       }
+//     })
+//     // With that done
+//     .then(function (data) {
+//       // Log the response
+//       console.log(data);
+//       // Empty the notes section
+//       $("#notes").empty();
+//     });
+
+//   // Also, remove the values entered in the input and textarea for note entry
+//   $("#titleinput").val("");
+//   $("#bodyinput").val("");
+// });
