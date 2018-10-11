4d9502feab951f432f43812e35b1a3d935085ed6
byt3_h34d <mr.tybopp@gmail.com>
2018-10-10 19:43:40 -0500

    minor formating changes

--- server.js
+++ server.js
@@ -43,7 +43,10 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
 
 // Routes
 
